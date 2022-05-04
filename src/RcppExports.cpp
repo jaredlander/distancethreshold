181c89e06b22253dd592781826d2bfd84a825e41
Jared Lander <info@jaredlander.com>
2022-05-04 18:27:17 -0400

    Made sure expand_column_values can handle objects that are vector-like but not quite vectors, such as timestamps.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -9,6 +9,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // dftomat
 arma::mat dftomat(DataFrame obj, CharacterVector cols);
 RcppExport SEXP _distancethreshold_dftomat(SEXP objSEXP, SEXP colsSEXP) {
